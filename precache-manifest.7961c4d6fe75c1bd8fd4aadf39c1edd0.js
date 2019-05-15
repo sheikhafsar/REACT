b5748e8fe18ee82c425b051c30549b3e0aaffd57
sheikhafsar <sheikhafsar72@gmail.com>
2019-05-15 18:01:36 +0530

    Updates

---
+++ precache-manifest.7961c4d6fe75c1bd8fd4aadf39c1edd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21155b55421d6cac9ea0a6c83d257451",
+    "url": "/REACT/index.html"
+  },
+  {
+    "revision": "5ca603b06381e3066b60",
+    "url": "/REACT/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/REACT/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "5ca603b06381e3066b60",
+    "url": "/REACT/static/js/main.7d661604.chunk.js"
+  },
+  {
+    "revision": "082a9fa1beb8b470403d",
+    "url": "/REACT/static/js/runtime~main.cb6da97c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/REACT/static/media/logo.5d5d9eef.svg"
+  }
+]);
