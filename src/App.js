1153277655d89379522b284256b229e9fa6268d4
Afsar Sheikh <sheikhafsar72@gmail.com>
2019-05-24 01:04:11 +0530

    form complete

--- src/App.js
+++ src/App.js
@@ -2,29 +2,17 @@ import React,{ Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Table from './Table'
+import Form from './Form'
 
 
 class App extends Component {
 
   state={
-    characters: [
-      {
-        name: 'Charlie',
-        job: 'Janitor',
-      },
-      {
-        name: 'Mac',
-        job: 'Bouncer',
-      },
-      {
-        name: 'Dee',
-        job: 'Aspring actress',
-      },
-      {
-        name: 'Dennis',
-        job: 'Bartender',
-      },
-    ],
+    characters: [],
+  }
+
+  handleSubmit = character => {
+    this.setState({ characters: [...this.state.characters, character] })
   }
 
   removeCharacter = index => {
@@ -42,6 +30,7 @@ class App extends Component {
     return (
       <div className="container">
         <Table characterData={characters} removeCharacter={this.removeCharacter} />
+        <Form handleSubmit={this.handleSubmit} />
       </div>
     );
   }  
